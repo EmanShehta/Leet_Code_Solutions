94393b3e36fb16485acef0a969b409b81985b71f
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2023-03-20 21:17:06 +0200

    Time: 24 ms (27.09%), Space: 20.3 MB (76.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,22 +1,24 @@
-class Solution {
+class Solution
+{
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0 ;
-        int s = flowerbed.size();
-        for(int i = 0 ; i < s ; i++ ){
-            if(flowerbed[i] == 0){      // when a place is
-                bool leftflag = (i == 0) || (flowerbed[i-1] == 0);  //check whether left is empty 
-                bool rightflag = (i == s-1) || (flowerbed[i+1] == 0); //check whether left is empty 
-                
-                if(leftflag && rightflag){  // updation
-                    flowerbed[i] = 1;
-                    count++;
-                    if(count >= n){
-                        return true;
-                    }
-                }
+    bool canPlaceFlowers(vector<int> &flowerbed, int n)
+    {
+        int N = flowerbed.size();
+        int count = 0;
+        int i = 0;
+        while (i < N)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == N - 1 || flowerbed[i + 1] == 0))
+            {
+                flowerbed[i++] = 1;
+                count++;
             }
+            if (count >= n)
+                return true;
+
+            i++;
         }
-        return count>=n;
+
+        return false;
     }
 };
