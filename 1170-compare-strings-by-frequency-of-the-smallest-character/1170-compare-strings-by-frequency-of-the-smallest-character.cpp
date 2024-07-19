fb0886e5102f800f2b5db4b723e4de37a5eb0f0c
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2024-07-19 15:09:00 +0300

    Time: 7 ms (87.54%), Space: 15.4 MB (51.32%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-     vector<int> numSmallerByFrequency(std::vector<std::string>& queries, std::vector<std::string>& words) {
-        std::vector<int> flscq;
-        std::vector<int> flscw;
-
-        for (const auto& x : queries) {
-            int frequency = std::count(x.begin(), x.end(), *std::min_element(x.begin(), x.end()));
-            flscq.push_back(frequency);
+    int lstocc(char ch, string word)
+    {
+        int n=word.length();
+        int lo=0, hi=n-1, mid;
+        int ans;
+        while(lo<=hi)
+        {
+            mid=(lo+hi)/2;
+            if(word[mid]==ch)
+            {
+                if(mid==n-1 || word[mid]!=word[mid+1])
+                return mid;
+                else
+                {
+                    lo=mid+1;
+                }
+            }
+            else if(word[mid]>ch)
+            {
+                hi=mid-1;
+            }
+            else
+            {
+                lo=mid+1;
+            }       
         }
-
-        for (const auto& x : words) {
-            int frequency = std::count(x.begin(), x.end(), *std::min_element(x.begin(), x.end()));
-            flscw.push_back(frequency);
+        return 0;
+    }
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        int n=words.size();
+        vector<int>freq(n);
+        for(int i=0;i<n;i++)
+        {
+            sort(words[i].begin(), words[i].end()); //n log n 
+            int lst= lstocc(words[i][0], words[i]); // log n
+            freq[i]=lst+1;
         }
-
-        std::vector<int> result;
-        for (int fq : flscq) {
-            int count = std::count_if(flscw.begin(), flscw.end(), [fq](int fw) {
-                return fq < fw;
-            });
-            result.push_back(count);
+        sort(freq.begin(), freq.end());
+        int m=queries.size();
+        vector<int>result(m);
+        for(int i=0;i<m;i++)
+        {
+            sort(queries[i].begin(), queries[i].end());
+            int lst=lstocc(queries[i][0], queries[i]);
+            auto it= upper_bound(freq.begin(), freq.end(), lst+1);
+            if(it==freq.end())
+            result[i]=0;
+            else
+            result[i]=n-(it-freq.begin());
         }
-
         return result;
     }
 };
