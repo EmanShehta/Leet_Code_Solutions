15ee7f2ed8ca58bb921df9a1b29e66c5b4f56934
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2024-07-30 17:12:14 +0300

    Time: 88 ms (98.06%), Space: 98.4 MB (49.89%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     using int2 = pair<int, int>;
     array<vector<int2>, 26> adj;
-           int D[26][26];
+           int arr[26][26];
 
     void Dijkstra(int x) {
-        int* dist=D[x];
+        int* dist=arr[x];
         priority_queue<int2, vector<int2>, greater<int2>> pq(adj[x].begin(), adj[x].end());
         while (!pq.empty()) {
             auto [d, i] = pq.top();
@@ -23,22 +23,22 @@ public:
     }
 
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-    fill(&D[0][0], &D[0][0] + 26 * 26, INT_MAX);
+    fill(&arr[0][0], &arr[0][0] + 26 * 26, INT_MAX);
     for(int i=0;i<26;i++)
     {
-       D[i][i]=0;
+       arr[i][i]=0;
     }
          const int sz = original.size();
  for(int i=0;i<sz;i++)
     {
         int row=original[i]-'a';
         int col=changed[i]-'a';
-        D[row][col]=min(D[row][col], cost[i]);
+        arr[row][col]=min(arr[row][col], cost[i]);
     }
      for (int i=0; i < 26; i++) {
             for (int j = 0; j < 26; j++) {
-                if (D[i][j] != INT_MAX && i!= j) {
-                    adj[i].emplace_back(D[i][j], j);
+                if (arr[i][j] != INT_MAX && i!= j) {
+                    adj[i].emplace_back(arr[i][j], j);
                 }
             }
         }
@@ -50,9 +50,15 @@ public:
         for (int i = 0; i < n; i++) {
             int row = source[i] - 'a';
             int col = target[i] - 'a';
-            if (D[row][col] == INT_MAX) return -1;
-            ans += D[row][col];
+            if (arr[row][col] == INT_MAX) return -1;
+            ans += arr[row][col];
         }
         return ans;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
