8d60ec97b4c8cf71efda7c30294a2fe6a3c8984a
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2024-08-03 16:59:28 +0300

    Time: 4 ms (92.55%), Space: 17.8 MB (32.70%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        int n=target.size(),m=arr.size();
-        if(n!=m)return false;
-      vector<int> ans(1002) ;
-        for(int i=0;i<n;i++)
-        {
+    bool canBeEqual(std::vector<int>& target, std::vector<int>& arr) {
+        int n = target.size();
+        int m = arr.size();
+
+        if (n != m) {
+            return false;
+        }
+
+        std::vector<int> ans(1001, 0);
+
+        for (int i = 0; i < n; i++) {
             ans[target[i]]++;
             ans[arr[i]]--;
         }
-        for(int i=0;i<n;i++)
-        {
-            if(ans[target[i]]!=0 || ans[arr[i]]!=0)
-            {
+
+        for (int i = 0; i < n; i++) {
+            if (ans[target[i]] != 0 || ans[arr[i]] != 0) {
                 return false;
             }
         }
+
         return true;
     }
     
