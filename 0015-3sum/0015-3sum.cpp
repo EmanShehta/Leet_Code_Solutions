22f366a3fe7589e200dde2902ab810aa40a3c75c
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2024-09-13 18:02:37 +0300

    Time: 56 ms (83.22%), Space: 27.4 MB (91.19%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size(); i++){
+            if(i>0&& nums[i]==nums[i-1])
+            {
+                continue;
+            }
+           int j=i+1;
+           int  k=nums.size()-1;
+            while(j<k)
+            {
+                int sm=nums[i]+nums[j]+nums[k];
+                if(sm==0)
+                {
+                res.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+                
+                while (j < k && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+                
+                while (j < k && nums[k] == nums[k + 1]) {
+                    k--;
+                }
+                }
+                else if(sm<0)
+                {
+                   j++; 
+                }
+                else 
+                {
+                    k--;
+                }
+            }
+        }
+       
+        return res;
+    }
+};
+auto init = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
