3218a6f5f98eddd98cba8963aa96c92e70af9caa
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2025-04-22 12:37:31 +0200

    0 ms (100%) 12.3 MB (11.85%) 0781-rabbits-in-forest .cpp Medium

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> freq;
+        for (int a : answers) {
+            freq[a]++;
+        }
+
+        int count = 0;
+        for (auto& [k, v] : freq) {
+            int groupSize = k + 1;
+            int groups = (v + k) / groupSize;
+            count += groups * groupSize;
+        }
+
+        return count;
+    }
+};
