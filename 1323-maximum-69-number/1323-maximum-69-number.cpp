a44fc19400b459c1a76cb66ca9b839770234ecf5
eman shehta <101083328+EmanShehta@users.noreply.github.com>
2025-08-17 01:18:25 +0300

    0 ms (100%) 8 MB (41.8%) 1323-maximum-69-number .cpp Easy

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        for( int i=1000;i>=1;i/=10)
+        {
+            if((num/i)%10==6)
+            {
+                num+=3*i;
+                break;
+            }
+        }
+        return num;
+        
+    }
+};
